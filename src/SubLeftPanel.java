1bfd2268b835ca9f2b7c947b6d21f30007792747
MOHDDANISHKHAN06 <47732298+MOHDDANISHKHAN06@users.noreply.github.com>
2022-11-29 22:51:08 -0700

    Update SubLeftPanel.java

--- src/SubLeftPanel.java
+++ src/SubLeftPanel.java
@@ -4,37 +4,50 @@ import javax.swing.JTextArea;
 
 public class SubLeftPanel extends JTextArea {
     String classNameText;
-    ArrayList<String> relationsText;
+    ArrayList<String> association;
     Box box;
-    int rows, cols;
+    ArrayList<String> inheritance;
+    ArrayList<String> composition;
 
-    public SubLeftPanel() {
+
+    public SubLeftPanel(int rows, int cols) {
+        super(rows, cols);
         setLineWrap(true);
-        relationsText = new ArrayList<>();
+        association = new ArrayList<>();
+        inheritance = new ArrayList<>();
+        composition = new ArrayList<>();
     }
 
-    public SubLeftPanel setSubLeftPanelRows(int rows) {
-        super.setRows(rows);
-        return this;
-    }
-
-    public SubLeftPanel setSubLeftPanelCols(int cols) {
-        super.setColumns(cols);
-        return this;
-    }
-
-    public String getRelationsText() {
+    public String getAssociationText() {
         String relations = "";
-        for (String s : relationsText) {
+        for (String s : association) {
             relations = relations + "\n" + s + "\n";
+        }
+        return relations;
+    }
+    public String getInheritanceText() {
+    	if(inheritance.size()==0) return "";
+        String relations = " extends ";
+        for (String s : inheritance) {
+            relations = relations + s ;
+        }
+        return relations;
+    }
+
+    public String getCompositionText() {
+        String relations = "";
+        for (String s : composition) {
+            relations = relations + s + "\n";
         }
         return relations;
     }
 
     public void setCodeText() {
-        String generatedString = "class " + classNameText + " { \n" + getRelationsText() + "\n}";
+    	
+        String generatedString = "class " + classNameText + getInheritanceText() + " { \n" + getCompositionText() + getAssociationText() + "\n}";
         setText(generatedString);
     }
+    
 
     public String getClassNameText() {
         return classNameText;
@@ -44,10 +57,21 @@ public class SubLeftPanel extends JTextArea {
         this.classNameText = classNameText;
     }
 
-    public void setRelationsText(String relationsText) {
+    public void setAssociationText(String relationsText) {
         String assocaitoString = "\t method() {\n \t\t" + relationsText + "\n \t}";
-        this.relationsText.add(assocaitoString);
+        this.association.add(assocaitoString);
     }
+    
+    public void setInheritanceText(String relationsText) {
+        String inherittoString = relationsText;
+        this.inheritance.add(inherittoString);
+    }
+    
+    public void setCompositionText(String relationsText) {
+        String compositetoString = relationsText;
+        this.composition.add(compositetoString);
+    }
+    
 
     public Box getBox() {
         return box;
@@ -55,15 +79,6 @@ public class SubLeftPanel extends JTextArea {
 
     public void setBox(Box box) {
         this.box = box;
-    }
-
-    @Override
-    public int getRows() {
-        return rows;
-    }
-
-    public int getCols() {
-        return cols;
     }
 
 }
