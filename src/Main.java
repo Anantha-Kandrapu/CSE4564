3410f3b8f770e7d776455a293fbbeb0c14acfed5
Loka Kalyan Balla <lballa@asu.edu>
2022-11-30 00:54:05 -0700

    added popups for menu items

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,41 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
         JMenu file, help;
-        JMenuItem New, save, load;
+        JMenuItem New, save, load, about;
         JFrame frame = new JFrame("SplitPaneFrame");
         JMenuBar mb = new JMenuBar();
         file = new JMenu("File");
-        help = new JMenu("help");
+        help = new JMenu("Help");
+        about = new JMenuItem("About");
+        about.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JOptionPane.showMessageDialog(frame, "                    Team: \n Anantha Ramayya Kandarpu  " +
+                        "\n Paromita Roy \n Mohammed Danish Khan \n Shivanjay Wagh \n Pradeep Nagisetti \n " +
+                        "Loka kalyan Balla");
+            }
+        });
         New = new JMenuItem("New");
         save = new JMenuItem("Save");
+        save.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                String fileName = JOptionPane.showInputDialog(frame,
+                        "Please Enter FileName?", null);
+            }
+        });
         load = new JMenuItem("Load");
+        load.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                String Name = JOptionPane.showInputDialog(frame,
+                        "Please Enter FileName?", null);
+            }
+        });
         file.add(New);
         file.add(save);
         file.add(load);
+        help.add(about);
         mb.add(file);
         mb.add(help);
         LeftPanel lp = new LeftPanel();
