9bf36cc014c07d6f877ed524897c447f2879a9a6
MOHDDANISHKHAN06 <47732298+MOHDDANISHKHAN06@users.noreply.github.com>
2022-11-29 23:52:02 -0700

    changes comp position

--- src/CompostionDecoration.java
+++ src/CompostionDecoration.java
@@ -10,13 +10,13 @@ public class CompostionDecoration extends JustLineDecorator {
 
     public void drawLine(int x1, int y1, int x2, int y2) {
         Graphics2D g2d = (Graphics2D) RightPanel.rightPanel.getGraphics();
-        super.drawLine(x1, y1, x2, y2);
+        super.drawLine(x1-10, y1+15, x2, y2+15);
         // for (int i = 0; i < 4; ++i) {
         // System.out.println("X : " + ix[i] + " Y :" + iy[i]);
         // }
 
         g2d.setColor(Color.BLACK);
-        g2d.drawRect(x1 - 15, y1 - 15, 15, 15);
-        g2d.fillRect(x1 - 15, y1 - 15, 15, 15);
+        g2d.drawRect(x1-15, y1 + 15, 15, 15);
+        g2d.fillRect(x1-15, y1 + 15, 15, 15);
     }
 }
